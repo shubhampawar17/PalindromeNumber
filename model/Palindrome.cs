ac88bffff6b68bd197db1f4404191c9846198994
shubhampawar17 <sp1180253@gmail.com>
2024-09-25 14:32:18 +0530

    Add project files.

---
+++ model/Palindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber.model
+{
+    internal class Palindrome
+    {
+        public static bool IsPalindrome(int num)
+        {
+            int rev = 0, temp = num;
+
+            while (num > 0)
+            {
+                int rem = num % 10;
+                rev = (rev * 10) + rem;
+                num /= 10;
+            }
+            return temp == rev;
+        }
+    }
+}
